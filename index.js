d19818cc5da070981116b33196836fca29a0a892
eFellow404 <123653222+eFellow404@users.noreply.github.com>
2025-05-29 19:41:00 +0100

    added dark theme
    added a dark theme button and changed the scaling so it works better on mobile

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const politics = document.getElementById('Politics');
+const button = document.getElementById('themechange');
+const header = document.getElementById('news-spinner');
 
+button.addEventListener('click', function() {
+    document.body.classList.toggle('darkbody');
+    header.classList.toggle('dark-news-spinner');
+});
